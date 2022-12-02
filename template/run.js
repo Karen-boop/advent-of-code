75469bda23455ffca8dda23bfd491340bc2dc3a2
karen-boop <karen.mccrann@shopify.com>
2022-12-02 22:52:23 +0000

    day2

---
+++ template/run.js
@@ -0,0 +1,14 @@
+import fs from 'fs'
+
+const lines = fs.readFileSync('input.txt', 'utf8').toString().split('\n')
+
+const part1 = () => {
+
+}
+
+const part2 = () => {
+
+}
+
+console.log('Part 1:', part1())
+console.log('Part 2:', part2())
